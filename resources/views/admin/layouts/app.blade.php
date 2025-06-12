9d1359b95be3e31d390bc3d24afea6e40dc15154
TienHoang472005 <kurosama0407@gmail.>
2025-06-12 22:32:07 +0700

    them chuc nang quan ly danh muc san pham

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{ asset('admin/css/icons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/css/app.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-PvNcazjx3bDAzjlfKEXAMPLEKEY..." crossorigin="anonymous" referrerpolicy="no-referrer" />
     @stack('styles')
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\Product\CategoryController;
 
 
 // Chuyển hướng trang chủ vào dashboard
@@ -12,4 +13,10 @@ Route::get('/', function () {
 // Admin routes
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Categories routes
+    Route::get('products/categories/trashed', [CategoryController::class, 'trashed'])->name('products.categories.trashed');
+    Route::post('products/categories/{id}/restore', [CategoryController::class, 'restore'])->name('products.categories.restore');
+    Route::delete('products/categories/{id}/force-delete', [CategoryController::class, 'forceDelete'])->name('products.categories.force-delete');
+    Route::resource('products/categories', CategoryController::class)->names('products.categories');;
 });
