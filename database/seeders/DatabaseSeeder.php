8fce4bb33d8f046881997694e727432946146e23
Dungtran2609 <trandung26092000@gmail.com>
2025-07-05 19:13:32 +0700

    Push code mới

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,89 +11,68 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        
-        
 
-        $this->call([ 
+
+
+        $this->call([
             UserSeeder::class,
-            CategorySeeder::class,
-            NewsCategorySeeder::class,
-            NewsSeeder::class,
-            NewsCommentSeeder::class,
-            LoginHistorySeeder::class,
-            ProductSeeder::class,
-
             RoleSeeder::class,
             PermissionSeeder::class,
-            UserSeeder::class,
             UserRoleSeeder::class,
             PermissionRoleSeeder::class,
 
-
-            // Thông báo
-            NotificationSeeder::class,
-            UserNotificationSeeder::class,
-
-            // Cấu trúc sản phẩm
             CategorySeeder::class,
             BrandSeeder::class,
+
+            NewsCategorySeeder::class,
+            NewsSeeder::class,
+            NewsCommentSeeder::class,
+
             ProductSeeder::class,
             ProductVariantSeeder::class,
 
-            // Thuộc tính sản phẩm
             AttributeSeeder::class,
             AttributeValueSeeder::class,
             ProductVariantAttributeSeeder::class,
 
-            // Tag & comment & view
             TagSeeder::class,
             ProductTagSeeder::class,
             ProductCommentSeeder::class,
             ProductViewSeeder::class,
             ProductAllImageSeeder::class,
 
-            // Kho
             WarehouseSeeder::class,
             WarehouseInventorySeeder::class,
 
-            // Địa chỉ, giao hàng, voucher
             UserAddressSeeder::class,
             ShippingMethodSeeder::class,
             CouponSeeder::class,
 
-            // Đơn hàng
             OrderSeeder::class,
             OrderItemSeeder::class,
             OrderReturnSeeder::class,
 
-            // Giao dịch
             TransactionSeeder::class,
-
-            // Wishlist
             WishlistSeeder::class,
 
-            // Bài viết
-            NewsSeeder::class,
-            NewsCommentSeeder::class,
-
-            // Báo cáo, cài đặt
             ActivityLogSeeder::class,
             ReportTopProductSeeder::class,
             ReportSalesDailySeeder::class,
             ReportUserActivitySeeder::class,
             SettingSeeder::class,
 
-            // Banner
+            NotificationSeeder::class,
+            UserNotificationSeeder::class,
+
             BannerSeeder::class,
-
-            // Lịch sử đăng nhập
             LoginHistorySeeder::class,
-
-
-
-
-
-
         ]);
+
+
+
+
+
+
+
     }
 }
--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -19,22 +19,28 @@ class NewsSeeder extends Seeder
     {
         DB::table('news')->insert([
             [
+                'category_id'=> '1',
                 'title' => 'Giới thiệu sản phẩm mới',
                 'content' => 'Chúng tôi hân hạnh giới thiệu sản phẩm mới nhất của năm 2025...',
+                'image'=> 'https://picsum.photos/100',
                 'author_id' => 1, 
                 'status' => 'published',
                 'published_at' => Carbon::now(),
             ],
             [
+                'category_id'=> '2',
                 'title' => 'Khuyến mãi lớn dịp hè',
                 'content' => 'Đừng bỏ lỡ cơ hội sở hữu sản phẩm với mức giá ưu đãi...',
+                'image'=> 'https://picsum.photos/100',
                 'author_id' => 1,
                 'status' => 'draft',
-                'published_at' => null,
+                'published_at' => Carbon::now(),
             ],
             [
+                'category_id'=> '3',
                 'title' => 'Cập nhật chính sách bảo hành',
                 'content' => 'Chính sách bảo hành mới sẽ mang lại nhiều quyền lợi hơn cho khách hàng...',
+                'image'=> 'https://picsum.photos/100',
                 'author_id' => 1,
                 'status' => 'published',
                 'published_at' => Carbon::now()->subDays(3),
